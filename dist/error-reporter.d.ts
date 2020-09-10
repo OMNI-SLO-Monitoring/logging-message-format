9765bef5a2ca53a4bad7b04598aa7c9b6d3d5e0e
Jonas Wallmann <jonaswallmann@hotmail.de>
2020-09-10 12:06:07 +0200

    tsc

--- dist/error-reporter.d.ts
+++ dist/error-reporter.d.ts
@@ -5,7 +5,9 @@ import ErrorFormat from "./error-format";
  * If error.correlationId is null, a new correlationId is created.
  * If error.correlationId is not null, the error will be reported with this correlationId
  *
- * @param error that sould be reported to the error monitor
+ * @param error that should be reported to the error monitor
+ * @param errorMonitorUrl the url of the error-monitor, where the error should be send to
+ *
  * @return ErrorFormat that was reported
  */
-export declare const reportError: (error: ErrorFormat) => ErrorFormat;
+export declare const reportError: (error: ErrorFormat, errorMonitorUrl: any) => ErrorFormat;
--- dist/error-reporter.js
+++ dist/error-reporter.js
@@ -9,16 +9,18 @@ const axios_1 = require("axios");
  * If error.correlationId is null, a new correlationId is created.
  * If error.correlationId is not null, the error will be reported with this correlationId
  *
- * @param error that sould be reported to the error monitor
+ * @param error that should be reported to the error monitor
+ * @param errorMonitorUrl the url of the error-monitor, where the error should be send to
+ *
  * @return ErrorFormat that was reported
  */
-exports.reportError = (error) => {
+exports.reportError = (error, errorMonitorUrl) => {
     if (error.correlationId == null) {
         error.correlationId = uuid_1.v4();
     }
     console.log("reporting error to monitor", error);
-    axios_1.default.post("http://localhost:3400", error)
-        .then(res => console.log("Error successfuly reported"))
+    axios_1.default.post(errorMonitorUrl, error)
+        .then(res => console.log("Error successfully reported"))
         .catch(err => console.log("Error when reporting error"));
     return error;
 };
